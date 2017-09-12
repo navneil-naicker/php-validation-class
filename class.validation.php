5ddfc90b18ab4f8e7efc25762a71d1fc0989fd30
Navneil Naicker <navneil.naicker@gmail.com>
2017-09-12 21:57:57 +1200

    Added custom validation rules using mask methods

--- class.validation.php
+++ class.validation.php
@@ -44,59 +44,91 @@ class cx_validate{
         }
     }
 
-}
+    public function name( $label, $value ){
+        if( !preg_match('/^([a-zA-Z]+[\'-]?[a-zA-Z]+[ ]?)+$/', $value) ){
+            return 'The ' . $label . ' field should only contain valid alphabetical characters.';
+        }
+    }
+
+    public function phone( $label, $value ){
+        $value = explode('-', $value);
+        $country = $value[0];
+        if( count($value) == 2 ){
+            $phone = $value[1];
+        } else if( count($value) == 3 ){
+            $area = $value[1];
+            $phone = $value[2];
+        }
+        if( strlen($country) < 1 or strlen($country) > 4 ){
+            return 'The ' . $label . ' field contains invalid country code.';
+        } else if( !preg_match("/^[0-9]+$/i", $country) ){
+            return 'The ' . $label . ' country code field can only have digits.';
+        } else if( count($value) == 3 and !preg_match("/^[0-9]+$/i", $area) ){
+            return 'The ' . $label . ' area code field can only have digits.';
+        } else if( !preg_match("/^[0-9]+$/i", $phone) ){
+            return 'The ' . $label . ' field can only have digits.';
+        }
+    }
+    
+    
 
 
-function cx_validate($request, $rules, $alts = array() ){
 
-    $v = new cx_validate;
-    $err = array();
 
-    if( count($rules ) ){
-        foreach($rules as $a => $b){
 
-            $alt = $v->_alt($alts, $a);
-            $c = explode('|', $b); $c = array_filter($c);
-            $m = count($c); $n = 1; $o = array();
-            $r = (!empty($request[$a]))?$request[$a]:'';
-            
-            foreach($c as $d => $e ){
-                if( empty($err[$a]) ){
-                    if( $e == 'required' and empty($r) ){
-                        if( !empty($v->required($alt, $r)) ){
-                            $err[$a] = $v->required($alt, $r);
-                        }
-                    } else if( strpos($e, ':') === false ){
-                        if( !empty($v->$e($alt, $r)) ){
-                            $err[$a] = $v->$e($alt, $r);
-                        }
-                   } else {
-                        $n++;
-                        list($h, $k) = explode(':', $e);
-                        $o[$h] = $k;
-                        if( $m == $n ){
-                            foreach($o as $p => $q ){
-                                if( $p == 'type' ){
-                                   if( !empty($v->$q($alt, $r)) ){
-                                        $err[$a] = $v->$q($alt, $r); break;
-                                    }
-                                } else {
-                                    if( !empty($v->$p($alt, $r, $q)) ){
-                                        $err[$a] = $v->$p($alt, $r, $q); break;
-                                    }
-                                }
+
+
+
+
+
+
+
+
+
+    public function validate($request, $rules, $alts = array() ){
+        $v = $this;
+        $err = array();
+        if( count($rules ) ){
+            foreach($rules as $a => $b){
+                $alt = $v->_alt($alts, $a);
+                $c = explode('|', $b); $c = array_filter($c);
+                $m = count($c); $n = 1; $o = array();
+                $r = (!empty($request[$a]))?$request[$a]:'';
+                foreach($c as $d => $e ){
+                    if( empty($err[$a]) ){
+                        if( $e == 'required' and empty($r) ){
+                            if( !empty($v->required($alt, $r)) ){
+                                $err[$a] = $v->required($alt, $r);
+                            }
+                        } else if( strpos($e, ':') === false ){
+                            if( !empty($v->$e($alt, $r)) ){
+                                $err[$a] = $v->$e($alt, $r);
                             }
                         } else {
+                            $n++;
+                            list($h, $k) = explode(':', $e);
                             $o[$h] = $k;
+                            if( $m == $n ){
+                                foreach($o as $p => $q ){
+                                    if( $p == 'type' ){
+                                        if( !empty($v->$q($alt, $r)) ){
+                                            $err[$a] = $v->$q($alt, $r); break;
+                                        }
+                                    } else {
+                                        if( !empty($v->$p($alt, $r, $q)) ){
+                                            $err[$a] = $v->$p($alt, $r, $q); break;
+                                        }
+                                    }
+                                }
+                            } else {
+                                $o[$h] = $k;
+                            }
                         }
                     }
                 }
             }
-
+            echo json_encode( $err );
         }
-
-        echo json_encode( $err );
-
     }
 
 }
--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 require_once( dirname(__FILE__) . '/class.validation.php');
 
 $request = $_POST;
-echo cx_validate($request, [
-    'name' => 'required|min:2|max:55',
+
+$cx_validate = new cx_validate;
+
+class cx_validate2 extends cx_validate{
+
+}
+
+echo $cx_validate->validate($request, [
+    'name' => 'required|min:2|max:55|type:name',
     'email' => 'required|min:5|max:55|type:email',
+    'phone' => 'required|type:phone',
     'website' => 'required|type:url',
     'message' => 'required|min:5|max:255|disallow_links',
 ]);
